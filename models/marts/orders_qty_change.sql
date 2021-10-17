19b3f359c249c68ff0ed09b0d4fac16b08ef20c1
zkid18 <daniel.chepenko@yahoo.com>
2021-10-17 16:49:53 +0300

    add crm

--- models/marts/orders_qty_change.sql
+++ models/marts/orders_qty_change.sql
@@ -1,9 +1,9 @@
 select 
-    date_trunc('week', load_date) AS first_dow_ts,
+    date_trunc('week', order_date) AS first_dow_ts,
     status AS status,
     count(*) AS orders_cnt
 from 
     {{ ref('sat_order_details') }}
 group by
-    date_trunc('week', load_date),
+    date_trunc('week', order_date),
     status
